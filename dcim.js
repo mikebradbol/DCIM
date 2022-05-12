1309f39bc310b07d0203cfa3c93d3feba9f38d08
[Michael Boliek] <[mikebradbol@gmail.com]>
2022-05-12 02:19:14 -0400

    firebase stuff

--- dcim.js
+++ dcim.js
@@ -15,3 +15,18 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });
+
+var apiKey = "6f4c1f8ab42b423690533ed601c69672";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.setRequestHeader("X-API-Key", apiKey);
+
+xhr.onreadystatechange = function(){
+ if(this.readyState === 4 && this.status === 200){
+  var json = JSON.parse(this.responseText);
+  console.log(json); //Gjallarhorn
+ }
+}
+
+xhr.send();
